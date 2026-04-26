1ec9c8d8ab58b6dcc0ab3f7806047665cad6181e
rabithahz-salwa <salwaeff@gmail.com>
2026-04-26 21:36:10 +0700

    Add: utils package with dummy module for import testing

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+# utils/__init__.py
+# Package marker for utils.
+# Berisi modules untuk preprocessing dan helper functions skripsi.
---
+++ utils/dummy_module.py
@@ -0,0 +1,17 @@
+"""
+Dummy module untuk test import.
+Akan dihapus setelah verify mekanisme import works.
+"""
+
+def say_hello(name):
+    """Return greeting message."""
+    return f"Halo {name}, AUTORELOAD WORKS! 🪄"
+
+
+def add_numbers(a, b):
+    """Simple addition for testing."""
+    return a + b
+
+
+# Variable juga bisa di-export
+SKRIPSI_NAME = "Temporal Topic Modeling Banking Superapp"
